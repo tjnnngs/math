c76b1fc0665070268fad97330cbdb937b033cb8e
TONYA JENNINGS <you@example.com>
2017-07-23 19:00:46 -0400

    math_app

---
+++ jsscripts_math.js
@@ -0,0 +1,5 @@
+function mathAlert() {
+	var numberOne = document.getElementById('num1').value;
+	var numberTwo = document.getElementById('num2').value;
+	var operator = document.getElementById('action').value;
+}
